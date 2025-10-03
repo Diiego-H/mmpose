c67baf39feaa3caa4d20cd13decc1067e3105c7d
Diiego-H <diegohernandezanton@gmail.com>
2025-10-03 12:22:16 +0000

    Parallel estimation using GPU

--- mmpose/apis/inference.py
+++ mmpose/apis/inference.py
@@ -132,14 +132,14 @@ def init_model(config: Union[str, Path, Config],
 
 
 def inference_topdown(model: nn.Module,
-                      img: Union[np.ndarray, str],
+                      imgs: np.ndarray,
                       bboxes: Optional[Union[List, np.ndarray]] = None,
                       bbox_format: str = 'xyxy') -> List[PoseDataSample]:
-    """Inference image with a top-down pose estimator.
+    """Inference images with a top-down pose estimator.
 
     Args:
         model (nn.Module): The top-down pose estimator
-        img (np.ndarray | str): The loaded image or image file to inference
+        img (np.ndarray): The loaded images to inference
         bboxes (np.ndarray, optional): The bboxes in shape (N, 4), each row
             represents a bbox. If not given, the entire image will be regarded
             as a single bbox area. Defaults to ``None``
@@ -157,31 +157,23 @@ def inference_topdown(model: nn.Module,
         init_default_scope(scope)
     pipeline = Compose(model.cfg.test_dataloader.dataset.pipeline)
 
+    # Default bboxes
     if bboxes is None or len(bboxes) == 0:
-        # get bbox from the image size
-        if isinstance(img, str):
-            w, h = Image.open(img).size
-        else:
-            h, w = img.shape[:2]
-
-        bboxes = np.array([[0, 0, w, h]], dtype=np.float32)
+        bboxes = []
+        for img in imgs:
+            h,w = img.shape[:2]
+            bboxes.append(np.array([0,0,w,h], dtype=np.float32))  
     else:
-        if isinstance(bboxes, list):
-            bboxes = np.array(bboxes)
-
         assert bbox_format in {'xyxy', 'xywh'}, \
             f'Invalid bbox_format "{bbox_format}".'
 
         if bbox_format == 'xywh':
             bboxes = bbox_xywh2xyxy(bboxes)
 
-    # construct batch data samples
+    # Construct batch data samples
     data_list = []
-    for bbox in bboxes:
-        if isinstance(img, str):
-            data_info = dict(img_path=img)
-        else:
-            data_info = dict(img=img)
+    for img, bbox in zip(imgs, bboxes):
+        data_info = dict(img=img)
         data_info['bbox'] = bbox[None]  # shape (1, 4)
         data_info['bbox_score'] = np.ones(1, dtype=np.float32)  # shape (1,)
         data_info.update(model.dataset_meta)
